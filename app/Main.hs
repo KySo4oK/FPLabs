ec4b3e8bc7c0c51b936f18d23ee074b666b56626
KySo4oK <prostorostuk08@gmail.com>
2020-04-27 22:37:29 +0300

    wrote some aux functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 import System.IO
 import Data.List
+import Data.List.Split
 
 main = do
     line <- getLine
     handle <- openFile (parseCommandForFile line) ReadMode
     contents <- hGetContents handle
-    putStr ((unlines . lines) contents)
+    putStr ( if isSubsequenceOf "load" line
+             then contents 
+             else parseForSelectColumns (line,(words contents)))
     hClose handle
     main
 
@@ -14,3 +17,15 @@ parseCommandForFile command = if isSubsequenceOf "load" command
                               else takeTableFromSelect command
 
 takeTableFromSelect content = last  (words content)
+
+parseForSelectColumns (command,fileLines) = 
+
+getListOfIndexes (command,listOfColumns) =  tail (takeWhile (/="FROM") (words command)) 
+
+findIndexOfListElem (column,listOfColumns) =  auxFindIndexOfListElem(column,listOfColumns,0)
+
+auxFindIndexOfListElem (column,listOfColumns,index) = if listOfColumns == []
+                                             then -1 
+                                             else if head listOfColumns == column
+                                                  then index
+                                                  else auxFindIndexOfListElem(column,(tail listOfColumns), index+1)
