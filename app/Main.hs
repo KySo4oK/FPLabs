8d75226e19e626cd31d4015c8b004093b5fd8b5a
KySo4oK <prostorostuk08@gmail.com>
2020-04-28 14:37:51 +0300

    now work with tsv to

--- app/Main.hs
+++ app/Main.hs
@@ -6,13 +6,17 @@ main = do
     line <- getLine
     handle <- openFile (parseCommandForFile line) ReadMode
     contents <- hGetContents handle
-    putStr ((unlines (getContent(line,(lines contents)))))
+    putStr ((unlines (getContent(line,(lines contents),(getSeparator line)))))
     hClose handle
     main
 
-getContent (line, fileLines) = if isSubsequenceOf "load" line
+getSeparator command = if isSubsequenceOf ".csv" command
+                        then ","
+                        else "\t"
+
+getContent (line, fileLines, sep) = if isSubsequenceOf "load" line
                                          then fileLines
-                                         else parseForSelectColumns(line,fileLines)
+                                         else parseForSelectColumns(line,fileLines,sep)
 
 parseCommandForFile command = if isSubsequenceOf "load" command
                               then (tail . dropWhile (/='(') . init) command
@@ -20,15 +24,15 @@ parseCommandForFile command = if isSubsequenceOf "load" command
 
 takeTableFromSelect content = last  (words content)
 
-parseForSelectColumns (command,fileLines) =
-      let listOfIndexes = getListOfIndexes ((getColumnsFromCommand command),(splitOn "," (head fileLines)))
-      in map (\line -> changeLine (listOfIndexes,line)) fileLines
+parseForSelectColumns (command,fileLines,sep) =
+      let listOfIndexes = getListOfIndexes ((getColumnsFromCommand command),(splitOn sep (head fileLines)))
+      in map (\line -> changeLine (listOfIndexes,line,sep)) fileLines
 
 getColumnsFromCommand command = map (delete ',') (tail (takeWhile (/="FROM") (words command)))
 
 
-changeLine (listOfIndexes,line) = let fields = splitOn "," line
-                                in intercalate ","(map (getElementByIndex fields) listOfIndexes)
+changeLine (listOfIndexes,line,sep) = let fields = splitOn sep line
+                                in intercalate sep (map (getElementByIndex fields) listOfIndexes)
 
 getElementByIndex myList index = if index == 0
                                then head myList
